80f21c664e7295bc15b1a3511f7ff90b7956c04e
ism21775750 <ism21775750@a01.informatica.escoladeltreball.org>
2018-01-31 11:48:59 +0100

    first commit

---
+++ bucle.py
@@ -0,0 +1,9 @@
+#coding: utf-8
+num1=input("Introduzca un numero:")
+num2=input("Introduzca un numero mayor:")
+
+while (num1<num2):
+	num2=input("Intentelo de nuevo:")
+	print "Los numeors que has esctiro son",num1,"y",num2
+
+
