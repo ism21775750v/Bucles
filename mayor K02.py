c698592baeda6e9faa4afbadda5b31e2bf2cda54
ism21775750 <ism21775750@a01.informatica.escoladeltreball.org>
2018-01-31 11:51:05 +0100

    first commit

---
+++ mayor K02.py
@@ -0,0 +1,9 @@
+#coding: utf-8
+num1=float(input("Introduzca un numero:"))
+num2=float(input("Introduzca un numero mayor:"))
+
+while (num1<num2):
+	num2=float(input("Introduzca un numero mayor que el primero:"))
+print num2 ,"no es mayor que", num1
+
+	
